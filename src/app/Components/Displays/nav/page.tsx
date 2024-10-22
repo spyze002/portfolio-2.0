58895efeee9c4d3ac4b1ba81f078d925c5ff9f4b
EBR <esum.brandon@adorsys.com>
2024-10-22 08:52:58 +0100

    updates.

--- src/app/Components/Displays/nav/page.tsx
+++ src/app/Components/Displays/nav/page.tsx
@@ -1,11 +1,13 @@
+import Image from 'next/image';
 import Link from "next/link";
+import LogoTrans from './Assets/Logo/logoTransp.png';
 import navStle from './navStyle.module.css';
 
 const NavBar = () => {
   return (
    <>
    <div className={navStle.main}>
-    <div> dev Logo</div>
+    <div> <Image src={LogoTrans} alt="main logo" width={100} className='mb-5' /> </div>
     <ul className={navStle.submain}>
       <li><Link href={'/'}>Home</Link></li>
       <li><Link href={'/bio'}>Bio</Link></li>
